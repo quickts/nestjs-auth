9b4a7ce28ffe3e9b28842eda4459a005442de020
chengaoyuan <chen.gaoyuan@qq.com>
2019-08-14 11:11:09 +0800

    增加AuthGlobalModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,22 @@ import { createAuthOptionProvider } from "./auth.provider";
 import { AuthService } from "./auth.service";
 import { JwtStrategy } from "./jwt.strategy";
 
-@Global()
 @Module({})
 export class AuthModule {
+    static forRoot(options: JwtModuleOptions | string): DynamicModule {
+        const optionProvider = createAuthOptionProvider(typeof options == "string" ? options : options.publicKey || options.secret);
+        return {
+            module: AuthModule,
+            providers: [optionProvider, AuthService, JwtStrategy],
+            exports: [AuthService],
+            imports: [PassportModule.register({}), JwtModule.register(typeof options == "string" ? { secret: options } : options)]
+        };
+    }
+}
+
+@Global()
+@Module({})
+export class AuthGlobalModule {
     static forRoot(options: JwtModuleOptions | string): DynamicModule {
         const optionProvider = createAuthOptionProvider(typeof options == "string" ? options : options.publicKey || options.secret);
         return {
