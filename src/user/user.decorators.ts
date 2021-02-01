91e0e15ac9c1d8199fe63be656610d29720cb4c1
ChenGaoYuan <chen.gaoyuan@qq.com>
2021-02-01 16:15:36 +0800

    更新nest到7.x

--- src/user/user.decorators.ts
+++ src/user/user.decorators.ts
@@ -1,12 +1,15 @@
-import { createParamDecorator } from "@nestjs/common";
-
-export const User = createParamDecorator((data, req) => {
-    if (!req.user) {
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { Request, Response } from 'express';
+export const User = createParamDecorator((data: string, context: ExecutionContext) => {
+    const http = context.switchToHttp();
+    const req = http.getRequest<Request>();
+    const user: any = req.user;
+    if (!user) {
         throw new Error(`Can't get user, you must use '@AuthCheck()'!`);
     }
     if (data) {
-        return req.user[data];
+        return user[data];
     } else {
-        return req.user;
+        return user;
     }
 });
